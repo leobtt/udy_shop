c2ae875c9bf45acaa6aa0f52e613fb7f1f68abb6
Leonardo Batista <leonarbat8@gmail.com>
2023-02-21 22:35:55 -0300

    fix: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => ProductList(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: theme.colorScheme.copyWith(
